d572c031e1702c24658bc1151fba9cd0e8755c69
Ian Agranovich <agranovich.ian@gmail.com>
2015-09-09 22:04:12 +0300

    one-step-ahead-mem --DONE

--- mem-heavy-garch.R
+++ mem-heavy-garch.R
@@ -6,27 +6,14 @@ require(xts)
 ## Import HEAVY estimators from EViews through CSV
 ##---------------------------------------------------------------------------
 
-## heavyStaticEstim      <- list()
-## heavyStaticEstim$sp   <- read.csv("./coef_2008/heavy_sp_coef.csv")
-#heavyStaticEstim$stox <- read.csv("./coef_2008/heavy_stox_coef.csv")
-
 heavy.static.coef.sp <- read.csv("./coef_2008/heavy_coef_sp.csv")
 heavy.static.coef.rs <- read.csv("./coef_2008/heavy_coef_rs.csv")
+mem.static.coef.sp <- read.csv("./coef_2008/mem_coef_sp.csv")
+mem.static.coef.rs <- read.csv("./coef_2008/mem_coef_rs.csv")
 
-##--------------------------------------------------------------------------
-## Get HEAVY coefficients from heavyStaticEstim
-##--------------------------------------------------------------------------
-
-
-## joinHeavyEstim  <- join_all(heavyStaticEstim, by = "Variable")
-## coefStaticHeavy <- joinHeavyEstim[ ,colnames(joinHeavyEstim) == "Coef"]
-## rownames(coefStaticHeavy) <- joinHeavyEstim$Variable
-## colnames(coefStaticHeavy) <- names(heavyStaticEstim)
-
-
-##
-## TODO: add discription
-##
+##---------------------------------------------------------------------------
+## HAVY forecast
+##---------------------------------------------------------------------------
 
 heavy.static.test.sp <-
     cbind(dtaTest[ ,"SPX2.openprice"],
@@ -57,6 +44,47 @@ heavy.static.test.rs <-
                                   coef     = heavy.static.coef.rs[3:4,"Coef"],
                                   coef.var = heavy.static.coef.rs[2,  "Coef"],
                                   data     = cbind(ind.rk, rcto) %>% head(-1)),
+           mse   = (RUT2.rk - garch)^2,
+           qlike = log(garch) - RUT2.rk / garch)
+
+##----------------------------------------------------------------------------
+## MEM forecast
+##----------------------------------------------------------------------------
+
+mem.static.test.sp <-
+    cbind(dtaTest[ ,"SPX2.openprice"],
+          dtaTest[ ,"SPX2.closeprice"],
+          dtaTest[ ,"SPX2.highlow"],
+          dtaTest[ ,"SPX2.rk"]) %>%
+    na.omit() %>%
+    data.frame() %>%
+    mutate(rcto  = log(SPX2.closeprice / SPX2.openprice),
+           hilo2 = SPX2.highlow^2,
+           garch = VarStaticForc(init.var = dtaEViewsSP$rcto %>% var(),
+                                 const    = mem.static.coef.sp[1,  "Coef"],
+                                 coef     = mem.static.coef.sp[3:5,"Coef"],
+                                 coef.var = mem.static.coef.sp[2,  "Coef"],
+                                 data     = cbind(SPX2.rk, hilo2, rcto) %>%
+                                            head(-1)),
+           mse   = (SPX2.rk - garch)^2,
+           qlike = log(garch) - SPX2.rk / garch)
+
+mem.static.test.rs <-
+    cbind(dtaTest[ ,"RUT2.openprice"],
+          dtaTest[ ,"RUT2.closeprice"],
+          dtaTest[ ,"RUT2.rk"]) %>%
+    na.omit() %>%
+    data.frame() %>%
+    mutate(rcto  = log(RUT2.closeprice / RUT2.openprice),
+           ind   = ifelse(rcto < 0, 1, 0),
+           ind_rcto2 = ind * rcto^2,
+           ind_rk = ind * RUT2.rk,
+           garch = VarStaticForc(init.var = dtaEViewsRUS$rcto %>% var(),
+                                 const    = mem.static.coef.rs[1,  "Coef"],
+                                 coef     = mem.static.coef.rs[3:5,"Coef"],
+                                 coef.var = mem.static.coef.rs[2,  "Coef"],
+                                 data     = cbind(ind_rcto2, rcto, ind_rk) %>%
+                                            head(-1)),
            mse   = (RUT2.rk - garch)^2,
            qlike = log(garch) - RUT2.rk / garch)
 
@@ -107,5 +135,23 @@ CheckStaticForc(heavy.static.test.rs[6,6],
                 c(heavy.static.test.rs[5,5],
                   heavy.static.test.rs[5,4]))
 
+
+CheckStaticForc(mem.static.test.sp[494,7],
+                mem.static.test.sp[493,7],
+                mem.static.coef.sp[1,"Coef"],
+                mem.static.coef.sp[3:5,"Coef"],
+                mem.static.coef.sp[2,"Coef"],
+                c(mem.static.test.sp[493,4],
+                  mem.static.test.sp[493,6],
+                  mem.static.test.sp[493,5]))
+
+CheckStaticForc(mem.static.test.rs[7,8],
+                mem.static.test.rs[6,8],
+                mem.static.coef.rs[1,"Coef"],
+                mem.static.coef.rs[3:5,"Coef"],
+                mem.static.coef.rs[2,"Coef"],
+                c(mem.static.test.rs[6,6],
+                  mem.static.test.rs[6,4],
+                  mem.static.test.rs[6,7]))
 
 
